5daa1a2a084f3167fa25a40ab42fbc52733c5a43
anket.singh <aniket.singh@yatra.com>
2026-06-04 16:00:50 +0530

    feat: project setup done

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app.js'
+import connectDB from './db/index.js';
+
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+.then(()=> {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.error('Failed to connect to the database', err);
+    process.exit(1);
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+export const asyncHander = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+  };
+};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error",
+//         });
+//     }
+// }
